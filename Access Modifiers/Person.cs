10b370b100c7aef6f04ed5299b1cdf65333c87c3
alexberg90 <ayazya@code.edu.az>
2022-11-20 15:13:59 +0400

    person

---
+++ Access Modifiers/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Access_Modifiers
+{
+    internal class Person
+    {
+        public Person()
+        {
+            
+        }
+        private string _name;
+        public string Name 
+        { 
+            
+            get 
+            {
+               
+                return _name;
+            } 
+            set 
+            {
+
+                if (value.Length>4)
+                {
+                    _name = value;
+                    Console.WriteLine("success");
+                    return;
+
+                }
+                Console.WriteLine("wrong");
+            } 
+        }
+    }
+}
---
+++ Access Modifiers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Access_Modifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+
+            Person person = new Person();
+            Console.WriteLine("enter the name");
+            person.Name = Console.ReadLine();
+            Console.WriteLine(person.Name);
+
+
+
+        }
+    }
+}
